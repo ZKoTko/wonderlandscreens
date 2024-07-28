61cf3ffada8893da224cef275e299f3a8c250b6f
KoTko (Konstantin Bagdasarov) <75670213+ZKoTko@users.noreply.github.com>
2024-07-29 02:33:53 +0400

    push

---
+++ assets/js/sliders.js
@@ -0,0 +1,56 @@
+$(document).ready(function(){
+    $('.restaurant-slider').slick({
+        dots: true,
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1
+    });
+});
+
+$(document).ready(function(){
+    $('.bar-slider').slick({
+        dots: true,
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1
+    });
+});
+
+$(document).ready(function(){
+    $('.team-slider').slick({
+        dots: true,
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1
+    });
+});
+
+$(document).ready(function(){
+    $('.choise-items-slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        centerMode: true,
+        centerPadding: '100px', 
+        focusOnSelect: true,
+        infinite: false, 
+        speed: 300,
+        initialSlide: 0,
+        dots: true,
+        vertical: false
+    });
+});
+
+$(document).ready(function(){
+    $('.choise-items-slider2').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        centerMode: true, 
+        centerPadding: '100px', 
+        focusOnSelect: true, 
+        infinite: false, 
+        speed: 300, 
+        initialSlide: 0,
+        dots: true,
+        vertical: false 
+    });
+});
